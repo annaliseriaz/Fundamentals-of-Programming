f0c96368e523db75ec33c7c798cc763db171fa41
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:10:08 +0800

    Zoe1_midterm.java

---
+++ Zoe1_midterm.java
@@ -0,0 +1,32 @@
+package midtermlab_activity;
+
+public class Zoe1_midterm {
+
+	public static void main(String[] args) {
+		 // Enter products & items
+        System.out.print("Enter Product 1: ");
+        System.out.println("Enter Price: ");
+
+        System.out.print("Enter Product 2: ");
+        System.out.println("Enter Price: ");
+
+        System.out.print("Enter Product 3: ");
+        System.out.println("Enter Price: ");
+
+        // Mode of payments
+        System.out.print("Cash Discounts: ");
+        System.out.println("\n You input 10.00% Discount For Cash");
+
+        System.out.println("3 Months Installments: ");
+        System.out.println("\n You input 5.00% Discount For Cash");
+        System.out.println("6 Months Installment: ");
+        System.out.println("12 Months Installment: ");
+
+        //Main Menu
+        System.out.print("Press I/i: ");
+        System.out.println("Press S/s: ");
+        System.out.println("Price X/x: ");
+
+
+	}
+}
