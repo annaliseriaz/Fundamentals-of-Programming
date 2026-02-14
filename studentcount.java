2f12d96eda8139e36901321b0fd0d2691be53e5f
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:07:56 +0800

    studentcount.java

---
+++ studentcount.java
@@ -0,0 +1,30 @@
+package methodsquizandsimulations;
+
+public class Studentcount {
+
+	public static void main(String[] args) {
+		int value;
+		int x;
+		x = 3;
+		System.out.print("A");
+		value = checkLength(x);
+		System.out.print("B");
+		System.out.print(x);
+		System.out.print(value);
+	}
+	public static void loadSong(int id)
+	{
+		System.out.print(id);
+	}
+	public static int checkLength(int id)
+	{
+		System.out.print(id);
+		id = id + 4;
+		System.out.print(id);
+		return 9; 
+	}
+	public static int getStudentCount()
+	{
+		return 6;
+	}
+}
