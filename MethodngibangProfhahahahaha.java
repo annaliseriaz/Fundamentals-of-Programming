e1a6e0b3d29c0a0828789abb0e6986c456836583
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:05:19 +0800

    MethodngibangProfhahahahaha.java

---
+++ MethodngibangProfhahahahaha.java
@@ -0,0 +1,27 @@
+package methodsquizandsimulations;
+import java.util.Scanner;
+
+public class MethodngibangProfhahahahaha {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number of eggs: ");
+		int numEggs = input.nextInt();
+		System.out.print("\nNumber of dozen: ");
+		int numDozen = input.nextInt();
+		System.out.print("\nEggs less than a dozen: ");
+		int eggsinaDozen = input.nextInt();
+		
+		showLessDozen();
+	}
+	
+	public static void showLessDozen() {
+		Scanner ilalagay = new Scanner(System.in);
+		System.out.print("\nEnter the number of eggs: ");
+		int bilangngItlog = ilalagay.nextInt();
+		System.out.print("\nNumber of dozen: ");
+		int ilangdosena = ilalagay.nextInt();
+		System.out.print("\nEggs less than a dozen: "); 
+		int itlogsaisangDosena = ilalagay.nextInt();
+	}
+}
