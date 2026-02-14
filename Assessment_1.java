f600b5454c2434d6788e5004869e26b0d5e6ed59
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 17:53:46 +0800

    Assessment_1.java

---
+++ Assessment_1.java
@@ -0,0 +1,28 @@
+package conditional_statements;
+public class Assessment_1 {
+
+	public static void main(String[] args) {
+		int a = 1, b = 4, c = 0;
+		
+		c = b + a*2;
+		b++;
+		a = c*(b-1) + 7%3;
+		System.out.println("Value of a is " + a);
+		System.out.println("Value of a is " + b);
+		System.out.println("Value of a is " + c);
+		
+		if (b>c)
+		{
+			a--;
+			c++;
+		}
+		else
+		{
+			b--;
+			b += 4;
+			c *= 3;
+		}
+		System.out.println();
+		System.out.println("Values of a, b, c are " + a + ", " + b + ", " + c + " respectively.");
+	}
+}
