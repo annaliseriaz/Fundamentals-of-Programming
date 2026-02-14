fe16103fe5d25f5df9aab224ad6bb82d76d3737c
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:14:40 +0800

    whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+package simulation_sourcecodes;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int i=1, n=4; 
+		int s=1, p=0;
+		while (i <= n)
+		{
+			s=i*i;
+			p+=s;
+			i++;
+		}
+		System.out.println("The value of s is " + s);
+		System.out.println("The value of p is " + p);
+	}
+}
