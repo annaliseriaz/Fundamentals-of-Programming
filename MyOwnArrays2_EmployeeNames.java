14b4c4c49bef4006930d84ffa1cd1ba4eb5baf34
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 17:53:03 +0800

    MyOwnArrays2_EmployeeNames.java

--- MyOwnArrays2_EmployeeNames.java
+++ MyOwnArrays2_EmployeeNames.java
@@ -1,7 +1,7 @@
 package arrays;
 import java.util.Scanner;
 import java.util.*;
-public class MyOwnArrays2_EmployeeNames {
+public class MyOwnArrays2_EmployeeNames { 
 
 	public static void main(String[] args) {
 		array();
