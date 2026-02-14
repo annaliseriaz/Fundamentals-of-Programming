1775251a032301edd65872000ae52998c337638c
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:00:04 +0800

    Methods2.java
    lesson code ni ser

---
+++ Methods2.java
@@ -0,0 +1,17 @@
+package lessoncodesnisir;
+
+public class Methods2 {
+
+	public static void main(String[] args) {
+		int num1=6, num2=7;
+		int a;
+		System.out.println("The value is "+ sum(num1,num2));
+		}
+	
+	public static int sum(int a, int b)
+	{
+		int s=0;
+		s=b+a; // where num1=a, and num2=b
+		return s;
+	}
+} 
