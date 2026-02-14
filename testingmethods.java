fb33f1d9d774d2f2a8bc817c4498bb63c4d8f33a
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 18:08:32 +0800

    testingmethods.java

---
+++ testingmethods.java
@@ -0,0 +1,13 @@
+package methodsquizandsimulations;
+
+public class TestingMethods2 {
+
+	void show() {
+		System.out.println("SHOW Method ");
+		return;
+	}
+	public static void main(String[] args) {
+		TestingMethods2 t2 = new TestingMethods2();
+		t2.show();
+	}
+}
